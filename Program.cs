76077a2d8f0b8396df049fd45d8c7b4ac7bfda09
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-09-26 00:18:21 -0300

    triangulo

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _AreaTrianguloRet_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Base;
+            double Altura; 
+            double Area;
+            
+            Console.Write("Valor da Base em (cm): ");
+            Base = double.Parse(Console.ReadLine());
+
+            Console.Write(" Valor da Altura em (cm): ");
+            Altura = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"A Area do Triangulo é: {Area = Base * Altura / 2} (cm²)");
+            Console.ForegroundColor = ConsoleColor.Green;
+             
+            
+
+
+            
+            
+
+            
+        }
+    }
+}
